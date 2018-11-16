06c3ae164859452c101ea338c1c73bbbe091d075
i3moreeekillua <44929293+i3moreeekillua@users.noreply.github.com>
2018-11-16 14:56:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame('s!help | *By iTzMoody','https://www.twitch.tv/peery13');
+ client.user.setGame('s!help | *By i3moreee','https://www.twitch.tv/peery13');
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -27,16 +27,16 @@ client.on('ready', () => {
   console.log('')
 });
 client.on('ready', async() => {
-var server = "499933149019766785"; 
-var channel = "499933149573283862";
+var server = "512873182198497300"; 
+var channel = "512873983553830913";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز ')
     },305);
 })
 
 client.on('ready', async() => {
-var server = "499933149019766785"; 
-var channel = "499933149573283862";
+var server = "512873182198497300"; 
+var channel = "512873983553830913";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#daily')
     },86400);
